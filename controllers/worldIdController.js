cf82b0d5a682c0183ed5e2e28c99350ed18cfab3
Kazuki Tobita <kazukiti201@gmail.com>
2020-09-21 03:35:58 +0900

    Modify generate world id api 400 response

--- controllers/worldIdController.js
+++ controllers/worldIdController.js
@@ -6,7 +6,7 @@ module.exports = {
   generateWorldId: (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({});
+      return res.status(400).json({msg: 'ぼしゅうにしっぱいしました'});
     }
 
     const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
