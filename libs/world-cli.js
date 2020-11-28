a0d287bd9246bdfeec80890b0bd23e6ead929312
Kazuki Tobita <kazukiti201@gmail.com>
2020-11-28 23:18:39 +0900

    Modify cli

--- libs/world-cli.js
+++ libs/world-cli.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch')
+const Table = require('cli-table3')
 
 require('dotenv').config()
 
@@ -8,11 +9,19 @@ const port = process.env.APP_PORT
 const endpoint = '/api/v1/states'
 
 const worldCli = async () => {
+  const table = new Table({
+    head: ['ID', 'Status', 'CreatedAt'],
+    colWidths: [10, 20, 30]
+  })
+
   const res = await fetch(`${scheme}://${host}:${port}${endpoint}`)
   const states = await res.json()
   for (const state of states) {
-    console.log(state)
+    table.push([
+      state.id, state.status, state.created_at
+    ])
   }
+  console.log(table.toString())
 }
 
 worldCli()
