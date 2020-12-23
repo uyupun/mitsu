920d1a862684c3ba240f1ddc5f8db44626dcd3b6
Kazuki Tobita <kazukiti201@gmail.com>
2020-12-23 15:09:24 +0900

    feat: add verify token middleware
    #105

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { check } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const fs = require('fs')
+const path = require('path')
 const authController = require('../controllers/auth-controller')
 const worldController = require('../controllers/world-controller')
 const rulesController = require('../controllers/rules-controller')
@@ -28,6 +31,36 @@ router.post('/login', [
 ], authController.login.bind(authController))
 
 /**
+ * ワールド情報の取得
+ * 現状、本番環境での使用はできないようにしている（認証とかめんどいので）
+ */
+if (process.env.NODE_ENV !== 'production') { router.get('/states', worldController.states.bind(worldController)) }
+
+/**
+ * トークンの検証ミドウェア
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) return res.status(400).json({})
+  if (req.headers.authorization.split(' ')[0] !== 'Bearer') return res.status(400).json({})
+  const token = req.headers.authorization.split(' ')[1]
+  const secretKey = fs.readFileSync(path.join(__dirname, '/../jwt_secret_key'), 'utf-8')
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) return res.status(401).json({})
+    next()
+  })
+}
+// TODO: とりあえずテスト環境ではバイパスしている
+if (process.env.NODE_ENV !== 'test') {
+  router.use('/rules', verifyToken)
+  router.use('/recruit', verifyToken)
+  router.use('/join', verifyToken)
+}
+
+/**
  * ルールの取得
  */
 router.get('/rules', rulesController.getRules.bind(rulesController))
@@ -46,11 +79,5 @@ router.get('/recruit', [
 router.get('/join', [
   check('worldId').not().isEmpty().isLength({ min: 6, max: 6 })
 ], worldController.join.bind(worldController))
-
-/**
- * ワールド情報の取得
- * 現状、本番環境での使用はできないようにしている（認証とかめんどいので）
- */
-if (process.env.NODE_ENV !== 'production') { router.get('/states', worldController.states.bind(worldController)) }
 
 module.exports = router
