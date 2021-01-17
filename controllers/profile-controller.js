2c7c6e67ea7b11471c54850bead29c41a4568807
takashi0602 <takashioicsn@gmail.com>
2021-01-17 17:04:35 +0900

    refactor: modify: profile api
    re #111

--- controllers/profile-controller.js
+++ controllers/profile-controller.js
@@ -2,6 +2,7 @@ const { validationResult } = require('express-validator')
 const models = require('../models')
 const fs = require('fs')
 const avatars = JSON.parse(fs.readFileSync('controllers/avatars.json', 'utf-8'))
+const ranks = JSON.parse(fs.readFileSync('controllers/ranks.json', 'utf-8'))
 
 class Profile {
   async getProfile (req, res, next) {
@@ -16,10 +17,11 @@ class Profile {
     if (!user) return res.status(400).json({ msg: 'プロフィールのしゅとくにしっぱいしました' })
 
     const avatar = avatars.find((avatar) => avatar.id === user.avatarType)
+    const rank = ranks.find((rank) => rank.rate.lower <= user.rate && rank.rate.upper >= user.rate)
     return res.status(200).json({
       avatar: avatar.image,
       rate: user.rate,
-      rank: '/images', // TODO: 画像取得
+      rank: rank.image,
       history: {
         win: user.win,
         lose: user.lose
