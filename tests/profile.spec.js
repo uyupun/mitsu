08cdb579cce20ed9474812aaf0d200962352c683
takashi0602 <takashioicsn@gmail.com>
2021-01-23 16:15:28 +0900

    feat: add: avatarId to profile api response
    re #111

--- tests/profile.spec.js
+++ tests/profile.spec.js
@@ -12,6 +12,7 @@ describe('get profile api', () => {
       const token = (await request(server).post('/api/v1/login').send({ userId: 'foo', password: 'password' })).body.token
       const res = await request(server).get('/api/v1/profile').set('Authorization', `Bearer ${token}`).query({ userId: 'foo' })
       expect(res.body).toMatchObject({
+        avatarId: expect.any(Number),
         avatar: expect.any(String),
         rate: expect.any(Number),
         rank: expect.any(String)
